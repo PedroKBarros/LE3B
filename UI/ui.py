11e6123dd86249bcbe9984685efa480054067062
Pedro Barros <pedrokatsbarros@gmail.com>
2021-06-06 01:20:30 -0300

    Partially added control bar
    Add images of the play button, add constants referring to the button,
    and add dimension parameters in the methods that handle mouse leave and
    mouse enter events.

--- UI/ui.py
+++ UI/ui.py
@@ -25,6 +25,12 @@ class Application:
         self.fileContainer2["padx"] = ui_consts.FILE_CONTAINER2_PADX
         self.fileContainer2["pady"] = ui_consts.FILE_CONTAINER2_PADY
 
+        self.controlsContainer3 = Frame(master)
+        self.controlsContainer3["bg"] = ui_consts.CONTROLS_BG_COLOR
+        self.controlsContainer3.pack()
+        self.controlsContainer3["padx"] = 100
+        self.controlsContainer3["pady"] = 2
+
         self.label1 = Label(self.fileContainer1)
         self.label1["bg"] = self.defaultBgColor
         self.label1["text"] = ui_consts.LABEL1_TEXT
@@ -43,27 +49,42 @@ class Application:
         self.load_image1 = self.load_image1.resize((50, 50), Image.ANTIALIAS) #Alterando as dimensões da imagem
         self.render_image1 = ImageTk.PhotoImage(self.load_image1)
 
-        self.button2 = Button(self.fileContainer2, image=self.render_image1)
-        self.button2.image = self.render_image1
+        self.button1 = Button(self.fileContainer2, image=self.render_image1)
+        self.button1.image = self.render_image1
+        self.button1["bd"] = 0 #Para definir a borda, tornando-a mínima
+        self.button1["highlightthickness"] = 0 #Para definir a espessura de destaque, retirando de fato a borda
+        self.button1.bind("<Enter>", lambda event, button=self.button1, imgName=ui_consts.IMAGE_PATH_BTN_OPEN_ENTER, size=(50, 50): self.handleEventMouseEnter(event, button, imgName, size))
+        self.button1.bind("<Leave>", lambda event, button=self.button1, imgName=ui_consts.IMAGE_PATH_BTN_OPEN_LEAVE, size=(50, 50): self.handleEventMouseLeave(event, button, imgName, size))
+        self.button1.bind("<Button-1>", self.handleEventMouseLeftClick)
+        self.button1.pack()
+
+        self.load_image2 = Image.open(ui_consts.IMAGE_PATH_BTN_PLAY_LEAVE)
+        self.load_image2 = self.load_image2.resize((20, 20), Image.ANTIALIAS) #Alterando as dimensões da imagem
+        self.render_image2 = ImageTk.PhotoImage(self.load_image2)
+
+        self.button2 = Button(self.controlsContainer3, image=self.render_image2)
+        self.button2.image = self.render_image2
         self.button2["bd"] = 0 #Para definir a borda, tornando-a mínima
         self.button2["highlightthickness"] = 0 #Para definir a espessura de destaque, retirando de fato a borda
-        self.button2.bind("<Enter>", lambda event, button=self.button2, imgName=ui_consts.IMAGE_PATH_BTN_OPEN_ENTER: self.handleEventMouseEnter(event, button, imgName))
-        self.button2.bind("<Leave>", lambda event, button=self.button2, imgName=ui_consts.IMAGE_PATH_BTN_OPEN_LEAVE: self.handleEventMouseLeave(event, button, imgName))
-        self.button2.bind("<Button-1>", self.handleEventMouseLeftClick)
-        self.button2.pack()
+        self.button2.bind("<Enter>", lambda event, button=self.button2, imgName=ui_consts.IMAGE_PATH_BTN_PLAY_ENTER, size=(20, 20): self.handleEventMouseEnter(event, button, imgName, size))
+        self.button2.bind("<Leave>", lambda event, button=self.button2, imgName=ui_consts.IMAGE_PATH_BTN_PLAY_LEAVE, size=(20, 20): self.handleEventMouseLeave(event, button, imgName, size))
+        self.button2.pack(side=RIGHT)
+
+        #self.list_box1 = Listbox(self.commentContainer3, selectmode=SINGLE)
+        #self.list_box1.pack()
         
 
-    def handleEventMouseEnter(self, event, button, imgName):
+    def handleEventMouseEnter(self, event, button, imgName, size):
         load_image2 = Image.open(imgName)
-        load_image2 = load_image2.resize((50, 50), Image.ANTIALIAS) #Alterando as dimensões da imagem
+        load_image2 = load_image2.resize(size, Image.ANTIALIAS) #Alterando as dimensões da imagem
         render_image2 = ImageTk.PhotoImage(load_image2)
         button.configure(image=render_image2)
         button.image = render_image2
         print("MOUSE ENTER!")
 
-    def handleEventMouseLeave(self, event, button, imgName):
+    def handleEventMouseLeave(self, event, button, imgName, size):
         load_image2 = Image.open(imgName)
-        load_image2 = load_image2.resize((50, 50), Image.ANTIALIAS) #Alterando as dimensões da imagem
+        load_image2 = load_image2.resize(size, Image.ANTIALIAS) #Alterando as dimensões da imagem
         render_image2 = ImageTk.PhotoImage(load_image2)
         button.configure(image=render_image2)
         button.image = render_image2
