a7ea3779c1ab2f7302283ceeda1f499dc654ddb2
Pedro Barros <pedrokatsbarros@gmail.com>
2021-06-06 12:04:18 -0300

    Changed Tkinter's layout manager and changed the control bar
    Change the layout manager from pick to place, allowing greater freedom
    in the placement of widgets.
    Remove the frames and, as a consequence, add the background of the
    controls bar (controls_bar.png).
    Add the time bar to the control bar (time_bar.png), as well as its mouse
    enter and mouse leave events.

--- UI/ui.py
+++ UI/ui.py
@@ -4,7 +4,12 @@ from tkinter.filedialog import askopenfilename
 from PIL import Image, ImageTk
 import UI.ui_constants as ui_consts
 
-def buildUI(master=None):
+def buildUI(root):
+    root.geometry('300x300')
+    root.resizable(False, False)
+    root.title(ui_consts.ROOT_TITLE + " " + ui_consts.VERSION)
+    root["bg"] = ui_consts.DEFAULT_BG_COLOR
+
     labelFont = (ui_consts.FONT_NAME, ui_consts.FONT_SIZE2)
     entryFont = (ui_consts.FONT_NAME, ui_consts.FONT_SIZE1)
     buttonFont = (ui_consts.FONT_NAME, ui_consts.FONT_SIZE2)
@@ -12,62 +17,83 @@ def buildUI(master=None):
     defaultFgColor = ui_consts.DEFAULT_FG_COLOR
     defaultBgColor = ui_consts.DEFAULT_BG_COLOR
 
-    fileContainer1 = Frame(master)
-    fileContainer1["bg"] = defaultBgColor
-    fileContainer1.pack()
-    fileContainer1["padx"] = ui_consts.FILE_CONTAINER1_PADX
-    fileContainer1["pady"] = ui_consts.FILE_CONTAINER1_PADY
+    #fileContainer1 = Frame(master)
+    #fileContainer1["bg"] = defaultBgColor
+    #fileContainer1.pack()
+    #fileContainer1["padx"] = ui_consts.FILE_CONTAINER1_PADX
+    #fileContainer1["pady"] = ui_consts.FILE_CONTAINER1_PADY
 
-    fileContainer2 = Frame(master)
-    fileContainer2["bg"] = defaultBgColor
-    fileContainer2.pack()
-    fileContainer2["padx"] = ui_consts.FILE_CONTAINER2_PADX
-    fileContainer2["pady"] = ui_consts.FILE_CONTAINER2_PADY
+    #fileContainer2 = Frame(master)
+    #fileContainer2["bg"] = defaultBgColor
+    #fileContainer2.pack()
+    #fileContainer2["padx"] = ui_consts.FILE_CONTAINER2_PADX
+    #fileContainer2["pady"] = ui_consts.FILE_CONTAINER2_PADY
 
-    controlsContainer3 = Frame(master)
-    controlsContainer3["bg"] = ui_consts.CONTROLS_BG_COLOR
-    controlsContainer3.pack()
-    controlsContainer3["padx"] = 100
-    controlsContainer3["pady"] = 2
+    #controlsContainer3 = Frame(master)
+    #controlsContainer3["bg"] = ui_consts.CONTROLS_BG_COLOR
+    #controlsContainer3.pack()
+    #controlsContainer3["padx"] = 170
+    #controlsContainer3["pady"] = 2
+    #controlsContainer3.grid(row=3, column=0)
 
-    label1 = Label(fileContainer1)
+    label1 = Label(root)
     label1["bg"] = defaultBgColor
     label1["text"] = ui_consts.LABEL1_TEXT
     label1["fg"] = defaultFgColor
     label1["font"] = labelFont
-    label1.pack()
+    label1.place(x=17, y=0)
 
-    entry1 = Entry(fileContainer1)
-    entry1["width"] = 30
+    entry1 = Entry(root)
+    entry1["width"] = 33
     entry1["font"] = entryFont
     entry1["state"] = "readonly" #O estado pode ser disable, normal ou readonly. 
                                         #Para escrita por código ou pelo usuário, é necessário ser "normal"
-    entry1.pack()
+    entry1.place(x=17, y=20)
 
     load_image1 = Image.open(ui_consts.IMAGE_PATH_BTN_OPEN_LEAVE)
     load_image1 = load_image1.resize((50, 50), Image.ANTIALIAS) #Alterando as dimensões da imagem
     render_image1 = ImageTk.PhotoImage(load_image1)
 
-    button1 = Button(fileContainer2, image=render_image1)
+    button1 = Button(root, image=render_image1)
     button1.image = render_image1
     button1["bd"] = 0 #Para definir a borda, tornando-a mínima
     button1["highlightthickness"] = 0 #Para definir a espessura de destaque, retirando de fato a borda
     button1.bind("<Enter>", lambda event, button=button1, imgName=ui_consts.IMAGE_PATH_BTN_OPEN_ENTER, size=(50, 50): handleEventMouseEnter(event, button, imgName, size))
     button1.bind("<Leave>", lambda event, button=button1, imgName=ui_consts.IMAGE_PATH_BTN_OPEN_LEAVE, size=(50, 50): handleEventMouseLeave(event, button, imgName, size))
     button1.bind("<Button-1>", lambda event, widget=entry1: handleEventMouseLeftClick(event, widget))
-    button1.pack()
+    button1.place(x=133, y=50)
 
-    load_image2 = Image.open(ui_consts.IMAGE_PATH_BTN_PLAY_LEAVE)
-    load_image2 = load_image2.resize((20, 20), Image.ANTIALIAS) #Alterando as dimensões da imagem
+    load_image2 = Image.open(ui_consts.IMAGE_PATH_CONTROLS_BAR)
+    load_image2 = load_image2.resize((296, 30), Image.ANTIALIAS) #Alterando as dimensões da imagem
     render_image2 = ImageTk.PhotoImage(load_image2)
 
-    button2 = Button(controlsContainer3, image=render_image2)
-    button2.image = render_image2
+    label2 = Label(root, image=render_image2)
+    label2.image = render_image2
+    label2.place(x=0, y=110)
+
+    load_image3 = Image.open(ui_consts.IMAGE_PATH_BTN_PLAY_LEAVE)
+    load_image3 = load_image3.resize((20, 20), Image.ANTIALIAS) #Alterando as dimensões da imagem
+    render_image3 = ImageTk.PhotoImage(load_image3)
+
+    button2 = Button(root, image=render_image3)
+    button2.image = render_image3
     button2["bd"] = 0 #Para definir a borda, tornando-a mínima
     button2["highlightthickness"] = 0 #Para definir a espessura de destaque, retirando de fato a borda
     button2.bind("<Enter>", lambda event, button=button2, imgName=ui_consts.IMAGE_PATH_BTN_PLAY_ENTER, size=(20, 20): handleEventMouseEnter(event, button, imgName, size))
     button2.bind("<Leave>", lambda event, button=button2, imgName=ui_consts.IMAGE_PATH_BTN_PLAY_LEAVE, size=(20, 20): handleEventMouseLeave(event, button, imgName, size))
-    button2.pack(side=RIGHT)
+    button2.place(x=10, y=117)
+
+    load_image4 = Image.open(ui_consts.IMAGE_PATH_TIME_BAR)
+    load_image4 = load_image4.resize((160, 6), Image.ANTIALIAS) #Alterando as dimensões da imagem
+    render_image4 = ImageTk.PhotoImage(load_image4)
+
+    label3 = Label(root, image=render_image4)
+    label3.image = render_image4
+    label3["bd"] = 0
+    label3["highlightthickness"] = 0
+    label3.bind("<Enter>", lambda event, button=label3, imgName=ui_consts.IMAGE_PATH_TIME_BAR, size=(160, 8): handleEventMouseEnter(event, button, imgName, size))
+    label3.bind("<Leave>", lambda event, button=label3, imgName=ui_consts.IMAGE_PATH_TIME_BAR, size=(160, 6): handleEventMouseLeave(event, button, imgName, size))
+    label3.place(x=40, y=124)
 
     #self.list_box1 = Listbox(self.commentContainer3, selectmode=SINGLE)
     #self.list_box1.pack()
@@ -102,8 +128,6 @@ def printEntry(wgEntry, string):
 
 def executaUI():
     root = Tk()
-    root.resizable(False, False)
-    root.title(ui_consts.ROOT_TITLE + " " + ui_consts.VERSION)
     buildUI(root)
     root.mainloop()
     
