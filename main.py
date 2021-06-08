d94025e96c2009a7b099d6beb2801d6d4df79438
Pedro Barros <pedrokatsbarros@gmail.com>
2021-06-08 15:13:28 -0300

    Functions created in main.py reading comments and inserting them in the queue

--- main.py
+++ main.py
@@ -1,27 +1,58 @@
-from UI.ui import *
+import UI.ui_constants as ui_consts
+import UI.ui as ui
 import threading
 from collections import namedtuple, deque
 import main_constants as main_consts
+from random import randint
 
 filePath = ""
-Comment = namedtuple("Comment", "abbreviatedAuthorName colorAbbreviated authorName time text state")
+file = None
 commentsQueue = deque()
 
 def setFilePath(path):
+    global filePath
     filePath = path
 
 def commentsManagement():
-    load_thread = threading.Thread(target=loadComments)
+    load_thread = threading.Thread(target=loadCommentsManagement)
     load_thread.start()
 
+def loadCommentsManagement():
+    global filePath
+    global file
+    print(filePath)
+    try:
+        file = open(filePath, "r")
+        loadComments()
+    except(IOError, FileNotFoundError, FileExistsError):
+        ui.showErrorMsgBox(main_consts.EXCEPTION_MSG_TITLE, main_consts.FILE_EXCEPTION_MSG_TEXT)
+
 def loadComments():
-    pass
-    #try:
-    #file = open(filePath, "r")
-    #except(IOError, FileNotFoundError, FileExistsError):
-    #pass
-    #else: #Caso ocorra uma exceção, mas não seja nenhuma das definidas
-    
+    global commentsQueue
+    #Atenção: essa função considera que não é possível quebrar 
+    # linha em uma mensagem no chat do BigBlueButton
+    numLinha = 0
+    for linha in file:
+        if (numLinha == 0):
+            abbreviatedAuthorName = linha
+            abbreviatedAuthorName = abbreviatedAuthorName.rstrip("\n")
+        if (numLinha == 1):
+            authorName = linha
+            authorName = authorName.rstrip("\n")
+        if (numLinha == 2):
+            time = linha
+            time = time.rstrip("\n")
+        if (numLinha == 3):
+            text = linha
+            text = text.rstrip("\n")
+            colorAbbreviated = main_consts.COLORS_ABBREVIATED_AUTHOR_NAME[randint(0, 10)]
+            comment = {"abbreviatedAuthorName": abbreviatedAuthorName, 
+            "colorAbbreviated": colorAbbreviated, "authorName": authorName, "time": time, 
+            "text": text, "state": main_consts.COMMENT_STATES[0]}
+            commentsQueue.append(comment)
+            numLinha = -1
+
+        numLinha += 1    
 
 if __name__ == "__main__":
-    executaUI()
+    ui.executaUI()
--- main_constants.py
+++ main_constants.py
@@ -2,5 +2,9 @@ CREATED_STATE = "created"
 LOADED_STATE = "loaded"
 READING_STATE = "reading"
 READED_STATE = "read"
-
 COMMENT_STATES = (CREATED_STATE, LOADED_STATE, READING_STATE, READED_STATE)
+EXCEPTION_MSG_TITLE = "Oops!"
+FILE_EXCEPTION_MSG_TEXT = "Sorry! We were embarrassed that an error occurred with the text file."
+COLORS_ABBREVIATED_AUTHOR_NAME = ("#1565C0", "#6A1B9A", "#1A237E", "#311B92", "#4527A0", 
+                                  "#5E35B1", "#4A148C", "#01579B", "#3949AB", "#7B1FA2", 
+                                  "#283593")
